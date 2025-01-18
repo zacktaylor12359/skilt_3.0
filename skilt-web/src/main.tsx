0e31079ab9b98e8d07f2d93e7c1946219b281f5c
GrimbeardHalt <xiaofx@umich.edu>
2025-01-18 15:48:58 -0500

    components folder

--- skilt-web/src/main.tsx
+++ skilt-web/src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+// import ChallengeCard from "components/ChallengeCard";
+
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <ChallengeCard />
   </StrictMode>,
 )
